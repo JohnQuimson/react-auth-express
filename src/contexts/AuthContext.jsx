b7be11f5351973415795e3bc59c3ce5a3f2e9cbb
JohnQuimson <john.quimson777@gmail.com>
2024-06-25 15:33:26 +0200

    feat: input validation

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { Children, createContext, useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useStorage from '../hooks/useStorage';
+import axios from '../utils/axiosClient.js';
 
 const AuthContext = createContext();
 
@@ -10,9 +11,20 @@ const AuthProvider = ({ children }) => {
 
   const navigate = useNavigate();
 
-  const login = (payload, redirectTo) => {
-    setIsLoggedIn(true);
-    navigate(redirectTo || '/');
+  const login = async (payload, redirectTo) => {
+    try {
+      const { data: response } = await axios.post('/auth/login', payload);
+      console.log(response);
+      setIsLoggedIn(true);
+      navigate(redirectTo || '/');
+    } catch (err) {
+      const { errors } = err.response.data;
+      const error = new Error(
+        errors ? 'Credenziali errate' : err.response.data
+      );
+      error.errors = errors;
+      throw error;
+    }
   };
 
   const logout = () => {
